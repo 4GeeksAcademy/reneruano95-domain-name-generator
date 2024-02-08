0b8219751652c009d4ee771f97cfa46aaa3743c1
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-02-08 01:28:50 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,30 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".us", ".io"];
+
+  let text = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let l = 0; l < noun.length; l++) {
+        for (let k = 0; k < domain.length; k++) {
+          console.log(`${pronoun[i]}${adj[j]}${noun[l]}${domain[k]}`);
+
+          const para = document.createElement("li");
+          const node = document.createTextNode(
+            `${pronoun[i]}${adj[j]}${noun[l]}${domain[k]}`
+          );
+          para.appendChild(node);
+
+          const element = document.getElementById("list");
+          element.appendChild(para);
+        }
+      }
+    }
+  }
 };
